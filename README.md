88818c9329f4db16816539caf58f9f697dd82e13
fproldan <franciscoproldan@gmail.com>
2024-05-10 13:27:51 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Translations
+
+Example app to illustrate the usage of the docs translation action
+
+#### License
+
+MIT
---
+++ translations/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Translations",
+			"type": "module",
+			"label": _("Translations")
+		}
+	]
